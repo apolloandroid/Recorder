c08c51141079e284ef394c7a4eb5615dbfcb97df
apolloandroid <hmelevskih94@gmail.com>
2020-08-11 22:46:43 +0300

    Database DAO

---
+++ app/src/main/java/com/example/recorder/database/RecordDatabaseDao.kt
@@ -0,0 +1,31 @@
+package com.example.recorder.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface RecordDatabaseDao {
+    @Insert
+    fun insertRecord(record: RecordingItem)
+
+    @Update
+    fun updateRecord(record: RecordingItem)
+
+    @Query("SELECT * FROM recording_table WHERE id = :id")
+    fun getRecord(id: Long?): RecordingItem?
+
+    @Query("DELETE FROM recording_table")
+    fun clearAll()
+
+    @Query("DELETE FROM recording_table WHERE id = :id")
+    fun removeRecord(id: Long?)
+
+    @Query("SELECT * FROM recording_table ORDER BY id DESC")
+    fun getAllRecords():LiveData<List<RecordingItem>>
+
+    @Query("SELECT COUNT(*) FROM recording_table")
+    fun getCount():Int
+}
