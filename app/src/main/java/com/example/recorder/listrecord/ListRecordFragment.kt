c96199626fcf123b7efd7adb52ae580f67d997f4
apolloandroid <hmelevskih94@gmail.com>
2020-08-16 22:05:40 +0300

    Realised ListRecordFragment

--- app/src/main/java/com/example/recorder/listrecord/ListRecordFragment.kt
+++ app/src/main/java/com/example/recorder/listrecord/ListRecordFragment.kt
@@ -1,23 +1,37 @@
 package com.example.recorder.listrecord
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.recorder.R
+import com.example.recorder.database.RecordDatabase
+import com.example.recorder.databinding.FragmentListRecordBinding
 
 
 class ListRecordFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    @SuppressLint("FragmentLiveDataObserve")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list_record, container, false)
+        val binding: FragmentListRecordBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_list_record, container, false)
+        val application = requireNotNull(this.activity).application
+        val datasource = RecordDatabase.getInstance(application).recordDatabaseDao
+        val viewModelFactory = ListRecordViewModelFactory(datasource)
+        val listRecordViewModel = viewModelFactory.create(ListRecordViewModel::class.java)
+        binding.listRecordViewModel = listRecordViewModel
+        val adapter = ListRecordAdapter()
+        binding.recyclerView.adapter = adapter
+        listRecordViewModel.records.observe(this, Observer {
+            it.let { adapter.data = it }
+        })
+        return binding.root
     }
 }
