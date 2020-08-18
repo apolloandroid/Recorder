b5cd15c00f1d3293cf93943bf72b8a228f60a5db
apolloandroid <hmelevskih94@gmail.com>
2020-08-18 23:42:06 +0300

    Class for database testing

--- app/src/androidTest/java/com/example/recorder/RecordDatabaseTest.kt
+++ app/src/androidTest/java/com/example/recorder/RecordDatabaseTest.kt
@@ -3,9 +3,9 @@ package com.example.recorder
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.example.recorder.database.RecordDatabase
-import com.example.recorder.database.RecordDatabaseDao
-import com.example.recorder.database.RecordingItem
+import com.example.recorder.repository.database.Record
+import com.example.recorder.repository.database.RecordDatabase
+import com.example.recorder.repository.database.RecordDatabaseDao
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -35,8 +35,8 @@ class RecordDatabaseTest {
     @Test
     @Throws(Exception::class)
     fun testDatabase() {
-        recordDatabaseDao.insertRecord(RecordingItem())
-        val getCount = recordDatabaseDao.getCount()
+        recordDatabaseDao.insertRecord(Record())
+        val getCount = recordDatabaseDao.getCountRecords()
         Assert.assertEquals(getCount, 1)
     }
 }
