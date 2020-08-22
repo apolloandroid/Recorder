b54096c50139f647c9248359ab4f68c644a44ae3
apolloandroid <hmelevskih94@gmail.com>
2020-08-22 23:02:21 +0300

    Did some refactoring

--- app/src/main/java/com/example/recorder/record/RecordViewModelFactory.kt
+++ app/src/main/java/com/example/recorder/record/RecordViewModelFactory.kt
@@ -1,13 +1,13 @@
 package com.example.recorder.record
 
-import android.app.Application
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class RecordViewModelFactory (private val application: Application) : ViewModelProvider.Factory {
+class RecordViewModelFactory (private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(RecordViewModel::class.java)) {
-            return RecordViewModel(application) as T
+            return RecordViewModel(context) as T
         }
         throw   IllegalArgumentException("Unknown ViewModel class")
     }
