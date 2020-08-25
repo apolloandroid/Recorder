eaad68f1b415d6b34dcbfe7a3a3a7da12270be2e
apolloandroid <hmelevskih94@gmail.com>
2020-08-25 22:29:13 +0300

    Module for injecting RecordFragment.kt

--- app/src/main/java/com/example/recorder/di/RecordsFragmentModule.kt
+++ app/src/main/java/com/example/recorder/di/RecordsFragmentModule.kt
@@ -1,7 +1,17 @@
 package com.example.recorder.di
 
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.example.recorder.record.RecordViewModel
+import com.example.recorder.record.RecordViewModelFactory
 import dagger.Module
+import dagger.Provides
 
 @Module
-class RecordsFragmentModule {
+class RecordsFragmentModule(private val context: Context) {
+    @Provides
+    fun provideRecordsViewModel(): RecordViewModel {
+        val recordViewModelFactory = RecordViewModelFactory(context)
+        return recordViewModelFactory.create(RecordViewModel::class.java)
+    }
 }
