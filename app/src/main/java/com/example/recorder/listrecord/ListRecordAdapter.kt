9bac692e61053d6b9394a0e788ad195a79fe77c7
apolloandroid <hmelevskih94@gmail.com>
2020-08-26 23:49:58 +0300

    Added data binding

--- app/src/main/java/com/example/recorder/listrecord/ListRecordAdapter.kt
+++ app/src/main/java/com/example/recorder/listrecord/ListRecordAdapter.kt
@@ -1,11 +1,10 @@
 package com.example.recorder.listrecord
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recorder.R
+import com.example.recorder.databinding.ListItemRecordBinding
 import com.example.recorder.repository.database.Record
 import java.util.concurrent.TimeUnit
 
@@ -16,34 +15,37 @@ class ListRecordAdapter : RecyclerView.Adapter<ListRecordAdapter.ViewHolder>() {
             notifyDataSetChanged()
         }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var vName: TextView = itemView.findViewById(R.id.text_file_name)
-        var vLength: TextView = itemView.findViewById(R.id.text_file_length)
-
-        companion object {
-            fun from(parent: ViewGroup): ViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val view: View = layoutInflater.inflate(R.layout.list_item_record, parent, false)
-                return ViewHolder(view)
-            }
-        }
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val recordingItem = data[position]
+        holder.bind(recordingItem)
+    }
+
     override fun getItemCount() = data.size
 
+    class ViewHolder private constructor(binding: ListItemRecordBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        var recordName: TextView = binding.textFileName
+        var recordLength: TextView = binding.textFileLength
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val context = holder.itemView.context
-        val recordingItem = data[position]
-        val itemDuration = recordingItem.length
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(itemDuration)
-        val seconds =
-            TimeUnit.MILLISECONDS.toSeconds(itemDuration) - TimeUnit.MINUTES.toSeconds(minutes)
-        holder.vName.text = recordingItem.name
-        holder.vLength.text = String.format("%02d:%02d", minutes, seconds)
+        fun bind(currentRecord: Record) {
+            val recordDuration = currentRecord.length
+            val minutes = TimeUnit.MILLISECONDS.toMinutes(recordDuration)
+            val seconds =
+                TimeUnit.MILLISECONDS.toSeconds(recordDuration) - TimeUnit.MINUTES.toSeconds(minutes)
+            recordName.text = currentRecord.name
+            recordLength.text = String.format("%02d:%02d", minutes, seconds)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemRecordBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
     }
 }
