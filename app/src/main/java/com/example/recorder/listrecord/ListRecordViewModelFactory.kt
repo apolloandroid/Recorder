fed748dc907f59be84c5d86fd78335c52cb424c3
apolloandroid <hmelevskih94@gmail.com>
2020-08-27 23:29:52 +0300

    Edited constructor parameters

--- app/src/main/java/com/example/recorder/listrecord/ListRecordViewModelFactory.kt
+++ app/src/main/java/com/example/recorder/listrecord/ListRecordViewModelFactory.kt
@@ -1,15 +1,16 @@
 package com.example.recorder.listrecord
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.recorder.repository.database.RecordDatabaseDao
+import com.example.recorder.repository.Repository
 import java.lang.IllegalArgumentException
 
-class ListRecordViewModelFactory(private val databaseDao: RecordDatabaseDao) :
+class ListRecordViewModelFactory(private val context: Context, private val repository: Repository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ListRecordViewModel::class.java)) {
-            return ListRecordViewModel(databaseDao) as T
+            return ListRecordViewModel(context, repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
