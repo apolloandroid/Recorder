c5de18ba685afd54a59de5e4d3507cb3bac9cb3b
apolloandroid <hmelevskih94@gmail.com>
2020-08-27 23:30:13 +0300

    Edited constructor parameters and added LiveData

--- app/src/main/java/com/example/recorder/listrecord/ListRecordViewModel.kt
+++ app/src/main/java/com/example/recorder/listrecord/ListRecordViewModel.kt
@@ -1,9 +1,22 @@
 package com.example.recorder.listrecord
 
+import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.recorder.repository.Repository
+import com.example.recorder.repository.database.Record
 import com.example.recorder.repository.database.RecordDatabaseDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
 
-class ListRecordViewModel(dataSource: RecordDatabaseDao) : ViewModel() {
-    val database = dataSource
-    val records = database.getAllRecords()
+class ListRecordViewModel(private val context: Context, private val repository: Repository) :
+    ViewModel() {
+
+     val records: LiveData<List<Record>> = getAllRecords()
+
+    private fun getAllRecords(): LiveData<List<Record>> {
+        return runBlocking { repository.getAllRecords() }
+    }
 }
